2d3f76a54994c5789e6846f71032229bc0be3186
Sazzad Hossain <44097425+sazzadhossainmahim@users.noreply.github.com>
2020-12-25 20:16:49 +0600

    Update Header.js

--- Component/Header.js
+++ Component/Header.js
@@ -1,15 +1,27 @@
-inport React from 'react';
-import 'Header.css';
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 
-function Header(){
-    <header class= "header-login-signup">
-        <div class="header-limiter">
-            <h1><a href="/">Coding<span>Cafe</span></a></h1>
-            {'Copyright 0 '}
-            {'coding Cafe'}
-            {new Date(),getFullYear()}
-            
-        </div>
-    </header>
+function Header() {
+    return (
+        <header class="header-login-signup">
+            <div class="header-limiter">
+                <h1><a href="/">Whistle<span>Blower</span></a></h1>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <a class="selected"><Link to="/">About Us</Link></a>
+                    <a><Link to="/">Emergency Contact</Link></a>
+                </nav>
+
+            <ul>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+            </ul>
+
+            </div>
+        </header>
+    )
+
 }
+export default Header
